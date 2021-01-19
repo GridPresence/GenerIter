c4387bd36eb8213b0979524106344c5c8f178d95
Jeremy Pavier <jeremy.pavier@hivehome.com>
2021-01-19 15:17:42 +0000

    Tested bumpversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="GenerIter",
-    version="0.0.19",
+    version="0.0.20",
     author="Thomas Jackson Park & Jeremy Pavier",
     author_email="generiter@gmx.com",
     description="A package for Generative Iterative music composition.",
