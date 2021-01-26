0de445d097aa1a220220087c258c8ca2e18ebf9a
Jeremy Pavier <grid.presence@gmx.com>
2021-01-26 12:11:40 +0000

    Refined the bumpversion config

---
+++ devdeploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -fr dist
+rm -fr *.egg-info
+
+bumpversion build --allow-dirty --verbose
+python3 -m setup sdist bdist_wheel
+python3 -m twine upload --verbose --repository pypi dist/*
