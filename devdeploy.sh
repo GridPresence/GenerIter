d5d9f979a377837f81f44f0df07d4ecccdeea77a
Jeremy Pavier <grid.presence@gmx.com>
2021-02-15 09:34:01 +0000

    Deployed fix"

--- devdeploy.sh
+++ devdeploy.sh
@@ -3,6 +3,6 @@
 rm -fr dist
 rm -fr *.egg-info
 
-bumpversion build --allow-dirty --verbose
+#bumpversion build --allow-dirty --verbose
 python3 -m setup sdist bdist_wheel
 python3 -m twine upload --verbose --repository pypi dist/*
